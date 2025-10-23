5b7d1bcbc8bf79f1b5ba409aee09458c734275a3
ParvezAhamed7 <parvezahamed700@gamil.com>
2025-10-23 21:33:35 +0530

    Updated utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,4 +5,4 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export const API_URL = 'https://student-backend-otjm.onrender.com';
+export const API_URL = 'https://studentloadtimefix-backend.onrender.com';
